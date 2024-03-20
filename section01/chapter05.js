95cd6844d346d91b171ed864da68d1cb2eabaf0d
Chamsol Yoon <dantert@gmail.com>
2024-03-20 16:43:06 +0900

    first commit

---
+++ section01/chapter05.js
@@ -0,0 +1,34 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+let nan = NaN;
+// console.log(1 * "hello");
+
+// 2. String Type
+let myName = "Herbert";
+let myLocation = "LA";
+let introduce = myName + myLocation;
+// console.log(introduce);
+
+let introduceText = `${myName} in ${myLocation}`;
+// console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (nothing inside)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
