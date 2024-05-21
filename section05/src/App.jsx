fbe8d4216210422b56c35d9b3d561ca88732b7da
Chamsol Yoon <dantert@gmail.com>
2024-05-21 09:05:36 +0900

    5.2 React 컴포넌트
    Changes to be committed:
    	modified:   section05/.eslintrc.cjs
    	modified:   section05/src/App.jsx
    	new file:   section05/src/components/Footer.jsx
    	new file:   section05/src/components/Header.jsx
    	new file:   section05/src/components/Main.jsx

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
+// Root Component
 function App() {
   return (
     <>
-      <h1>Hello React!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// Component function
+// 대문자로 시작해야 Component로 인식
+// function Header() {
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
