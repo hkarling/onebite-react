0399d10e8f1a932a5050108f43ca6ee27f362d0a
Chamsol Yoon <dantert@gmail.com>
2024-05-31 13:21:43 +0900

    8.2 UI 구현하기
    Changes to be committed:
    	modified:   section08/src/App.css
    	modified:   section08/src/App.jsx
    	new file:   section08/src/components/Editor.css
    	new file:   section08/src/components/Editor.jsx
    	new file:   section08/src/components/Header.css
    	new file:   section08/src/components/Header.jsx
    	new file:   section08/src/components/List.css
    	new file:   section08/src/components/List.jsx
    	new file:   section08/src/components/TodoItem.css
    	new file:   section08/src/components/TodoItem.jsx

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import "./Editor.css";
+
+const Editor = () => {
+  return (
+    <div className="Editor">
+      <input placeholder="New To-do..." />
+      <button>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ section08/src/components/List.jsx
@@ -0,0 +1,18 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input placeholder="Input keyword" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default List;
