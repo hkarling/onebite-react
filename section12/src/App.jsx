fd10270750d5cf0ae0e639c36af106bbd3e9f234
hkarling <dantert@gmail.com>
2024-06-15 01:00:27 +0900

    12.10-12 Home 페이지 구현하기
    Changes to be committed:
    	modified:   section12/src/App.jsx
    	new file:   section12/src/components/DiaryItem.css
    	new file:   section12/src/components/DiaryItem.jsx
    	new file:   section12/src/components/DiaryList.css
    	new file:   section12/src/components/DiaryList.jsx
    	modified:   section12/src/pages/Home.jsx

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -10,15 +10,21 @@ import NotFound from "./pages/NotFound";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-06-15").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-06-14").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-05-11").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -37,12 +43,12 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  const idRef = useRef(mockData.length + 1);
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -80,27 +86,6 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={() => {
-          onCreate(new Date().getDate(), 1, "Hello");
-        }}
-      >
-        일기 추가 테스트
-      </button>
-      <button
-        onClick={() => {
-          onUpdate(1, new Date().getDate(), 1, "Hello");
-        }}
-      >
-        일기 수정 테스트
-      </button>
-      <button
-        onClick={() => {
-          onDelete(1);
-        }}
-      >
-        일기 삭제 테스트
-      </button>
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
           <Routes>
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,8 +1,54 @@
-import { useSearchParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
+const getMonthlyDate = (pivotDate, data) => {
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  );
+  return data.filter(
+    (item) => item.createdDate >= beginTime && item.createdDate <= endTime
+  );
+};
 const Home = () => {
-  const [params, setParams] = useSearchParams();
-  return <div>Home</div>;
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyDate(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
+  );
 };
 
 export default Home;
