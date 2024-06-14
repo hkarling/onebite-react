20192292cee5bea6897b06c88ff7b0732c65a8b0
hkarling <dantert@gmail.com>
2024-06-14 13:22:13 +0900

    8.3-9 기능 구현
    Changes to be committed:
    	modified:   section08/src/App.jsx
    	modified:   section08/src/components/Editor.jsx
    	modified:   section08/src/components/List.jsx
    	modified:   section08/src/components/TodoItem.jsx

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,14 +1,62 @@
 import "./App.css";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+const mocData = [
+  {
+    id: 2,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 0,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mocData);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에서 targetId와 일치하는 id를 갖는 투두 아이템의 isDone변경
+    // 인수 : todo 배열에서 targetId와 일치하는 id를 가진 요소의 isDone이 반전된 배열
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소를 삭제한 배열
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,43 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilterData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      // 대소문자 구분 x
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="Input keyword" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="Input keyword"
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {getFilterData().map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
       </div>
     </div>
   );
