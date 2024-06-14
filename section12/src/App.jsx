ff63fe29d2cef6488ca8b64301157045ef6aea58
hkarling <dantert@gmail.com>
2024-06-14 22:16:24 +0900

    12.1-5 페이지 라우팅
    Changes to be committed:
    	new file:   section12/.eslintrc.cjs
    	new file:   section12/.gitignore
    	new file:   section12/README.md
    	new file:   section12/index.html
    	new file:   section12/package-lock.json
    	new file:   section12/package.json
    	new file:   section12/src/App.css
    	new file:   section12/src/App.jsx
    	new file:   section12/src/index.css
    	new file:   section12/src/main.jsx
    	new file:   section12/src/pages/Diary.jsx
    	new file:   section12/src/pages/Home.jsx
    	new file:   section12/src/pages/New.jsx
    	new file:   section12/src/pages/NotFound.jsx
    	new file:   section12/vite.config.js
    	new file:   section12/src/App.jsx
    	new file:   section12/src/pages/Diary.jsx
    	new file:   section12/src/pages/Home.jsx

---
+++ section12/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import NotFound from "./pages/NotFound";
+
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새로운 일기를 작성하는 New 페이지
+// 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
+function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        {/* // SPA로 동작하지 않는다
+        <a href="/">Home</a>
+        <a href="/new">new</a>
+        <a href="/diary">diary</a> */}
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/diary/:id" element={<Diary />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
