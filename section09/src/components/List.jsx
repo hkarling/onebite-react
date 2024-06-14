b280d567dc52ce3717036b9717a13a863519a828
hkarling <dantert@gmail.com>
2024-06-14 18:55:18 +0900

    9.1 useReducer
    Changes to be committed:
    	new file:   section09/.eslintrc.cjs
    	new file:   section09/.gitignore
    	new file:   section09/README.md
    	new file:   section09/index.html
    	new file:   section09/package-lock.json
    	new file:   section09/package.json
    	new file:   section09/src/App.css
    	new file:   section09/src/App.jsx
    	new file:   section09/src/components/Editor.css
    	new file:   section09/src/components/Editor.jsx
    	new file:   section09/src/components/Exam.jsx
    	new file:   section09/src/components/Header.css
    	new file:   section09/src/components/Header.jsx
    	new file:   section09/src/components/List.css
    	new file:   section09/src/components/List.jsx
    	new file:   section09/src/components/TodoItem.css
    	new file:   section09/src/components/TodoItem.jsx
    	new file:   section09/src/index.css
    	new file:   section09/src/main.jsx
    	new file:   section09/vite.config.js

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,46 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilterData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      // 대소문자 구분 x
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="Input keyword"
+      />
+      <div className="todos_wrapper">
+        {getFilterData().map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
