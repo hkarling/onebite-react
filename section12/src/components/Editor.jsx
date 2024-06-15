5ee118ec43e35819e460f17b286cc85ae1f64d6f
hkarling <dantert@gmail.com>
2024-06-15 11:28:45 +0900

    12.15 Edit 페이지 구현하기
    Changes to be committed:
    	modified:   section12/src/components/Editor.jsx
    	modified:   section12/src/pages/Edit.jsx

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,7 +1,7 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const emotionList = [
@@ -42,7 +42,7 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
@@ -50,10 +50,16 @@ const Editor = ({ onSubmit }) => {
   });
   const nav = useNavigate();
 
-  const onChangeInput = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
 
+  const onChangeInput = (e) => {
     let name = e.target.name;
     let value = e.target.value;
     if (name === "createdDate") {
