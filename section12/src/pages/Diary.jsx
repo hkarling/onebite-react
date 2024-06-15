93bd914a21d590daa9c2e125bdf962f6939013d0
hkarling <dantert@gmail.com>
2024-06-15 13:03:13 +0900

    12.18 배포 준비하기
    Changes to be committed:
    	modified:   section12/index.html
    	new file:   section12/public/favicon.ico
    	new file:   section12/public/thumbnail.png
    	new file:   section12/src/hooks/usePageTitle.jsx
    	modified:   section12/src/pages/Diary.jsx
    	modified:   section12/src/pages/Edit.jsx
    	modified:   section12/src/pages/Home.jsx
    	modified:   section12/src/pages/New.jsx

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -4,16 +4,20 @@ import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
 import { getStringedDate } from "../utils/getStringedDate";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Diary = () => {
   const params = useParams();
   const nav = useNavigate();
+  usePageTitle(`${params.id}번 일기`);
+
   const curDiaryItem = useDiary(params.id);
   if (!curDiaryItem) {
     return <div>데이터 로딩중</div>;
   }
   const { createdDate, emotionId, content } = curDiaryItem;
   const title = getStringedDate(new Date(createdDate));
+
   return (
     <div>
       <Header
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,13 +1,16 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
 
 const New = () => {
   const nav = useNavigate();
   const { onCreate } = useContext(DiaryDispatchContext);
+  usePageTitle("새 일기 쓰기");
+
   const onSubmit = (input) => {
     onCreate(input.createdDate.getTime(), input.emotionId, input.content);
     nav("/", { replace: true });
